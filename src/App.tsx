3b76fb11f500b0492639e2c44de40b3ca7ea0e77
Fabio Arazaki <fabioarazaki@gmail.com>
2022-12-17 14:06:16 +0700

    feat: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from "components/Header";
 import HomeScreen from "modules/Home/screens/Main";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <HomeScreen />
     </div>
   );
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>Hacker News List</h1>
+    </div>
+  );
+};
+export default Header;
