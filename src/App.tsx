9fe3ff7d170f6a04e2b3cbbe11fba7c92d39526d
Fabio Arazaki <fabioarazaki@gmail.com>
2022-12-17 19:37:52 +0700

    feat: applied styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import Header from "components/Header";
+import StyledProvider from "libs/styled-components";
 import HomeScreen from "modules/Home/screens/Main";
+import styled from "styled-components";
+
+const Layout = styled.div`
+  overflow-y: auto;
+  padding: 2rem 3rem;
+`;
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <HomeScreen />
+      <StyledProvider>
+        <Header />
+        <Layout>
+          <HomeScreen />
+        </Layout>
+      </StyledProvider>
     </div>
   );
 }
