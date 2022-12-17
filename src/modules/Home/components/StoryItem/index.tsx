d8dc1351941d56138a9531f1d1dd13d5fc344637
Fabio Arazaki <fabioarazaki@gmail.com>
2022-12-17 20:16:10 +0700

    feat: applied some styles

--- src/modules/Home/components/StoryItem/index.tsx
+++ src/modules/Home/components/StoryItem/index.tsx
@@ -2,18 +2,18 @@ import { ExternalLink } from "assets/svg";
 import { Container } from "./styles";
 import { StoryProps } from "./types";
 
-const StoryItem = ({ title, id, by, url }: StoryProps) => {
+const StoryItem = ({ title, by, url }: StoryProps) => {
   return (
     <Container>
       <h3>{title}</h3>
       <div>
-        <h4>Author: {by}</h4>
+        <label>Author: {by}</label>
         {url && (
           <a href={url} target="_blank" rel="noopener noreferrer">
             <ExternalLink
               height="25px"
               width="25px"
-              outline="#fff"
+              outline="#8e98a2"
               color="transparent"
             />
           </a>
--- src/modules/Home/components/StoryItem/styles.ts
+++ src/modules/Home/components/StoryItem/styles.ts
@@ -6,9 +6,14 @@ export const Container = styled.div`
   align-items: flex-start;
   flex-direction: column;
   margin-bottom: 1rem;
-  background-color: ${({ theme }) => theme.colors.primary[800]};
+  background-color: ${({ theme }) => theme.colors.background.light};
+  color: ${({ theme }) => theme.colors.text.dark};
   padding: 1rem 2rem;
   & > div {
+    & > label {
+      font-size: ${({ theme }) => theme.fontSizes.sm};
+      color: ${({ theme }) => theme.colors.dark[300]};
+    }
     width: 100%;
     display: flex;
     justify-content: space-between;
